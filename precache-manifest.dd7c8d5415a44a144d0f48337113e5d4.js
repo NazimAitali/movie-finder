20e195e4ff70d64a9414dacffc1575fc2e41b3a2
nazim <nazimaitali88@gmail.com>
2020-10-06 17:03:44 +0100

    Updates

---
+++ precache-manifest.dd7c8d5415a44a144d0f48337113e5d4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294ef6ffd4d86e64c23f943f0377b1d1",
+    "url": "/movie-finder/index.html"
+  },
+  {
+    "revision": "07addc295d1cfb6640db",
+    "url": "/movie-finder/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "32e38554aefebcb775b0",
+    "url": "/movie-finder/static/css/main.611e3b8a.chunk.css"
+  },
+  {
+    "revision": "07addc295d1cfb6640db",
+    "url": "/movie-finder/static/js/2.4b623fc3.chunk.js"
+  },
+  {
+    "revision": "6a3315418ff229eedef5f19694365f7f",
+    "url": "/movie-finder/static/js/2.4b623fc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32e38554aefebcb775b0",
+    "url": "/movie-finder/static/js/main.f7e90a0c.chunk.js"
+  },
+  {
+    "revision": "45b919b4251198f431ec",
+    "url": "/movie-finder/static/js/runtime-main.2b925a39.js"
+  },
+  {
+    "revision": "2e649de6a152a33a5f85a6825f265a29",
+    "url": "/movie-finder/static/media/bg.2e649de6.jpg"
+  },
+  {
+    "revision": "16e91833426d93608aed5ab038796d29",
+    "url": "/movie-finder/static/media/error.16e91833.png"
+  },
+  {
+    "revision": "2d1a2f3899a0024b2eccd157cb99c19c",
+    "url": "/movie-finder/static/media/logo.2d1a2f38.png"
+  },
+  {
+    "revision": "09a1fcf8d82cbe7d7252087da5ae8db9",
+    "url": "/movie-finder/static/media/no-data.09a1fcf8.png"
+  },
+  {
+    "revision": "87dd1e20a64e95ef315fcd4b68ae74ac",
+    "url": "/movie-finder/static/media/no-photo-available.87dd1e20.png"
+  }
+]);
